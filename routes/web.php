c57de7c37097c8e90dffbc1b313682ba6ba85558
rsakml <rossaakmalia66@gmail.com>
2022-06-06 04:38:25 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
