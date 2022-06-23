f13d6f971175d3a6757a13eb89be99e574f525db
= <dellajannata2112@gmail.com>
2022-06-23 13:34:32 +0700

    Update HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
+use App\Models\Pesanan;
+use App\Models\PesananDetails;
+use App\Models\Product;
+use App\Models\Supplier;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,6 +31,12 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view('home',['user' => $user]);
+        $count = Product::all()->count();
+        $count2 = Pesanan::all()->count();
+        $count3 = User::all()->count();
+        $count4 = PesananDetails::all()->count();
+        $count5 = Employee::all()->count();
+        $count6 = Supplier::all()->count();
+        return view('admin.main', compact('user','count','count2','count3','count4','count5','count6'));
     }
 }
