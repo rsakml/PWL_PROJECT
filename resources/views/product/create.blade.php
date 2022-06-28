1a83445cb69b5212785996e6b46b21160ea95efb
rsakml <rossaakmalia66@gmail.com>
2022-06-28 11:40:28 +0700

    tambah button kembali di input produk

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -46,6 +46,7 @@
                             <label for="stok">Stok</label>
                             <input type="stok" name="stok" class="form-control" id="stok" aria-describedby="stok" >
                         </div>
+                        <a class="btn btn-success mt-3" href="{{ route('product.index') }}">Kembali</a>
                     <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
